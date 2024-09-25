e2185806ad1cf3b45151e7bc836fc7c278b50c59
shunsk <ok.xmonad@gmail.com>
2024-09-25 18:56:11 +0900

    add telescope dir of vim config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,10 +16,11 @@ return {
             require('telescope').load_extension('fzf')
 
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'find cwd' })
+            vim.keymap.set('n', '<leader>fv', function() builtin.find_files({cwd = vim.fn.stdpath("config")})end, { desc = 'find neovim config' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'help tags' })
         end,
     }
 }
